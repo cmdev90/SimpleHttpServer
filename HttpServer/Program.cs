b796fcc9e54d7b5704dc2176fd4df4b8b9adf26a
Cherlton Millette <cherlton.millette@gmail.com>
2015-05-14 23:02:02 -0400

    Fixed incorrect return message on sample post handler.

--- HttpServer/Program.cs
+++ HttpServer/Program.cs
@@ -11,14 +11,15 @@ namespace SimpleWebServer
         {
             WebServer app = new WebServer(3001);
 
-            app.get("/test/hello", (HttpListenerRequest request)=> {
-                String param = request.QueryString["Username"];
-                return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", param);
+            app.get("/test/hello", (HttpListenerRequest request) =>
+            {
+                return "<XML><MESSAGE>It works!</MESSAGE></XML>";
             });
 
-            app.post("/test/hello", (HttpListenerRequest request) => {
-                String param = request.QueryString["Username"];
-                return string.Format("<HTML><BODY>My web page</BODY></HTML>", param);
+            app.post("/test/hello", (HttpListenerRequest request) =>
+            {
+                string param = request.QueryString["query"];
+                return string.Format("<XML><MESSAGE>Your query is below.</MESSAGE><QUERY>{0}</QUERY></XML>", param);
             });
             
             app.Start();
